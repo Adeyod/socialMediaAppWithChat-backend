741f15c96e8424bd199187312112adf9b4b1eaa6
Ayodeji Adebolu <ayodejiadebolu@gmail.com>
2024-03-19 10:40:44 +0100

    frontend url changed inside socket file and also in the index file

--- socket/socket.js
+++ socket/socket.js
@@ -6,7 +6,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: 'https://socialmediaappwithchat-frontend.onrender.com',
+    // origin: 'http://localhost:5173',
     methods: ['POST', 'GET'],
   },
 });
